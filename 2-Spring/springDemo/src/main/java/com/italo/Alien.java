2ebf7308f732f689f52d5fd2bf22babafc4f8665
Ítalo Lemos Gasparo <italolemos4@gmail.com>
2024-11-14 06:59:38 -0300

    progess

--- 2-Spring/springDemo/src/main/java/com/italo/Alien.java
+++ 2-Spring/springDemo/src/main/java/com/italo/Alien.java
@@ -1,10 +1,17 @@
 package com.italo;
 
-import java.beans.ConstructorProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Alien {
-
+	@Value("21")
 	private int age;
+
+	//@Autowired
+	//@Qualifier("com2")
 	private Computer com;
 
 	public Alien() {
@@ -31,6 +38,9 @@ public class Alien {
 		return com;
 	}
 
+
+	@Autowired
+	@Qualifier("laptop")
 	public void setCom(Computer com) {
 		this.com = com;
 	}
